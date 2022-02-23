4e8999cc9894bd5a193ca5ea83fc66215468e980
4face-studi0 <4face.studio@gmail.com>
2022-02-23 08:58:30 +0100

    Add ranged values tests to StatQueriesTest.kt

--- shuttle/database/src/test/kotlin/shuttle/database/StatQueriesTest.kt
+++ shuttle/database/src/test/kotlin/shuttle/database/StatQueriesTest.kt
@@ -25,27 +25,27 @@ class StatQueriesTest {
 
 
     @Test
-    fun `one time and one location, for every app`() = runTest {
+    fun `one time and one location, with exact values, for every app`() = runTest {
         // given
         insertApps(3)
         val expected = listOf(SecondAppId, FirstAppId, ThirdAppId)
 
         // when
         with(queries) {
-            insertTimeStat(FirstAppId, time = Time, count = 5)
-            insertLocationStat(FirstAppId, latitude = Latitude, longitude = Longitude, count = 4)
-            insertTimeStat(SecondAppId, time = Time, count = 6)
-            insertLocationStat(SecondAppId, latitude = Latitude, longitude = Longitude, count = 8)
-            insertTimeStat(ThirdAppId, time = Time, count = 2)
-            insertLocationStat(ThirdAppId, latitude = Latitude, longitude = Longitude, count = 1)
+            insertTimeStat(FirstAppId, time = ExactTime, count = 5)
+            insertLocationStat(FirstAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 4)
+            insertTimeStat(SecondAppId, time = ExactTime, count = 6)
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
+            insertLocationStat(ThirdAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 1)
 
             val result = findMostUsedAppsIds(
-                latitude = Latitude,
-                latitude_ = Latitude,
-                longitude = Longitude,
-                longitude_ = Longitude,
-                time = Time,
-                time_ = Time
+                latitude = ExactLatitude,
+                latitude_ = ExactLatitude,
+                longitude = ExactLongitude,
+                longitude_ = ExactLongitude,
+                time = ExactTime,
+                time_ = ExactTime
             ).executeAsList()
 
             // then
@@ -54,25 +54,25 @@ class StatQueriesTest {
     }
 
     @Test
-    fun `one or two of time and location, for every app`() = runTest {
+    fun `one or two of time and location, with exact values, for every app`() = runTest {
         // given
         insertApps(3)
         val expected = listOf(SecondAppId, FirstAppId, ThirdAppId)
 
         // when
         with(queries) {
-            insertTimeStat(FirstAppId, time = Time, count = 5)
-            insertLocationStat(SecondAppId, latitude = Latitude, longitude = Longitude, count = 8)
-            insertTimeStat(ThirdAppId, time = Time, count = 2)
-            insertLocationStat(ThirdAppId, latitude = Latitude, longitude = Longitude, count = 1)
+            insertTimeStat(FirstAppId, time = ExactTime, count = 5)
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
+            insertLocationStat(ThirdAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 1)
 
             val result = findMostUsedAppsIds(
-                latitude = Latitude,
-                latitude_ = Latitude,
-                longitude = Longitude,
-                longitude_ = Longitude,
-                time = Time,
-                time_ = Time
+                latitude = ExactLatitude,
+                latitude_ = ExactLatitude,
+                longitude = ExactLongitude,
+                longitude_ = ExactLongitude,
+                time = ExactTime,
+                time_ = ExactTime
             ).executeAsList()
 
             // then
@@ -81,23 +81,104 @@ class StatQueriesTest {
     }
 
     @Test
-    fun `one or zero of time and location, for every app`() = runTest {
+    fun `one or zero of time and location, with exact values, for every app`() = runTest {
         // given
         insertApps(3)
         val expected = listOf(SecondAppId, ThirdAppId, FirstAppId)
 
         // when
         with(queries) {
-            insertLocationStat(SecondAppId, latitude = Latitude, longitude = Longitude, count = 8)
-            insertTimeStat(ThirdAppId, time = Time, count = 2)
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
 
             val result = findMostUsedAppsIds(
-                latitude = Latitude,
-                latitude_ = Latitude,
-                longitude = Longitude,
-                longitude_ = Longitude,
-                time = Time,
-                time_ = Time
+                latitude = ExactLatitude,
+                latitude_ = ExactLatitude,
+                longitude = ExactLongitude,
+                longitude_ = ExactLongitude,
+                time = ExactTime,
+                time_ = ExactTime
+            ).executeAsList()
+
+            // then
+            assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `one time and one location, with range values, for every app`() = runTest {
+        // given
+        insertApps(3)
+        val expected = listOf(SecondAppId, FirstAppId, ThirdAppId)
+
+        // when
+        with(queries) {
+            insertTimeStat(FirstAppId, time = ExactTime, count = 5)
+            insertLocationStat(FirstAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 4)
+            insertTimeStat(SecondAppId, time = ExactTime, count = 6)
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
+            insertLocationStat(ThirdAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 1)
+
+            val result = findMostUsedAppsIds(
+                latitude = RangeStartLatitude,
+                latitude_ = RangeEndLatitude,
+                longitude = RangeStartLongitude,
+                longitude_ = RangeEndLongitude,
+                time = RangeStartTime,
+                time_ = RangeEndTime
+            ).executeAsList()
+
+            // then
+            assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `one or two of time and location, with range values, for every app`() = runTest {
+        // given
+        insertApps(3)
+        val expected = listOf(SecondAppId, FirstAppId, ThirdAppId)
+
+        // when
+        with(queries) {
+            insertTimeStat(FirstAppId, time = ExactTime, count = 5)
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
+            insertLocationStat(ThirdAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 1)
+
+            val result = findMostUsedAppsIds(
+                latitude = RangeStartLatitude,
+                latitude_ = RangeEndLatitude,
+                longitude = RangeStartLongitude,
+                longitude_ = RangeEndLongitude,
+                time = RangeStartTime,
+                time_ = RangeEndTime
+            ).executeAsList()
+
+            // then
+            assertEquals(expected, result)
+        }
+    }
+
+    @Test
+    fun `one or zero of time and location, with range values, for every app`() = runTest {
+        // given
+        insertApps(3)
+        val expected = listOf(SecondAppId, ThirdAppId, FirstAppId)
+
+        // when
+        with(queries) {
+            insertLocationStat(SecondAppId, latitude = ExactLatitude, longitude = ExactLongitude, count = 8)
+            insertTimeStat(ThirdAppId, time = ExactTime, count = 2)
+
+            val result = findMostUsedAppsIds(
+                latitude = RangeStartLatitude,
+                latitude_ = RangeEndLatitude,
+                longitude = RangeStartLongitude,
+                longitude_ = RangeEndLongitude,
+                time = RangeStartTime,
+                time_ = RangeEndTime
             ).executeAsList()
 
             // then
@@ -113,9 +194,15 @@ class StatQueriesTest {
 
     companion object TestData {
 
-        const val Time = 100L
-        const val Latitude = 20.0
-        const val Longitude = 30.0
+        const val ExactTime = 100L
+        const val RangeStartTime = 90L
+        const val RangeEndTime = 110L
+        const val ExactLatitude = 20.0
+        const val RangeStartLatitude = 10.0
+        const val RangeEndLatitude = 30.0
+        const val ExactLongitude = 30.0
+        const val RangeStartLongitude = 20.0
+        const val RangeEndLongitude = 40.0
 
         const val FirstAppId = "app 1"
         const val SecondAppId = "app 2"
