14840d5263f2040b469de8f94da4a5aea0b8138e
Davide Giuseppe Farella <37957092+4face-studi0@users.noreply.github.com>
2022-03-09 09:37:01 +0100

    Remove last commit check from commitAppVersion.sh
    Not needed as it will run only on tagged commits

--- scripts/commitAppVersion.sh
+++ scripts/commitAppVersion.sh
@@ -3,13 +3,6 @@ set -eo pipefail
 
 function main {
 
-  # Run on if last commit is not a release
-  local lastCommitMessage=$(git log -1 --pretty=%B)
-  if [[ $lastCommitMessage == [release]* ]]; then
-    echo "skipping, last commit is already a release"
-    exit 0
-  fi
-
   # Set Git credentials
   if [ -z "$GIT_EMAIL" ] || [ -z "$GIT_USERNAME" ] || [ -z "$GITHUB_TOKEN" ]; then
     echo "=> You must set the variables GIT_EMAIL, GIT_USERNAME and GITHUB_TOKEN to be able to commit and create release"
