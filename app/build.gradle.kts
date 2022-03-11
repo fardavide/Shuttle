1d994c434aeea54b2d4f93e3f5f49b3689c3fb06
4face-studi0 <4face.studio@gmail.com>
2022-03-11 17:19:19 +0000

    [release] 0.1.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,8 +6,8 @@ shuttleAndroid {
     useCompose()
     androidApp(
         id = "studio.forface.shuttle",
-        versionCode = 101,
-        versionName = "0.1.1"
+        versionCode = 102,
+        versionName = "0.1.2"
     )
 }
 
