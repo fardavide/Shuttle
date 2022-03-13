2fb60dcdf4403041fb20697c324c7a3f5a54b8d1
4face-studi0 <4face.studio@gmail.com>
2022-03-13 12:22:51 +0100

    Implement PermissionItem.kt

--- shuttle/permissions/src/main/kotlin/shuttle/permissions/resources/Strings.kt
+++ shuttle/permissions/src/main/kotlin/shuttle/permissions/resources/Strings.kt
@@ -4,35 +4,50 @@ internal object Strings {
 
     object Accessibility {
 
+        const val Name = "Accessibility service"
         const val Title = "Enable accessibility service"
-        const val Description = "The accessibility service will need to retrieve only package of the open " +
-            "application, in order to improve the suggestions"
-        const val Action = "Enable now"
+        const val Description = "Shuttle needs access to the Accessibility Service, in order to improve the " +
+            "suggestions.\nBy enabling it you consent Shuttle to retrieve your current open app and store it to the " +
+            "local database"
+        const val Action = "Enable accessibility service"
+        const val PermissionGrantedDescription = "Accessibility service is enabled"
+        const val PermissionNotGrantedDescription = "Accessibility service is not enabled"
     }
 
     object Location {
 
-        const val Action = "Allow permissions"
+        const val Action = "Allow location permissions"
 
         object Coarse {
 
+            const val Name = "Location permissions"
             const val Title = "Allow location permissions"
-            const val Description = "The location permissions is needed, in order to improve the suggestions, " +
-                "related to your current location"
+            const val Description = "Shuttle needs access to Location, in order to show suggested apps, based on " +
+                "your current location\nBy allowing the permission you consent Shuttle to store a Geo Hash of your " +
+                "current position and store it to the local database"
+            const val PermissionGrantedDescription = "Approximate location permission is granted"
+            const val PermissionNotGrantedDescription = "Approximate location permission is not granted"
         }
 
         object Fine {
 
+            const val Name = "Precise location permissions"
             const val Title = "Allow fine location permissions"
-            const val Description = "The precise location permissions is needed, because the approximate location " +
-                "has a too wide range to provide precise suggestions"
+            const val Description = "Shuttle needs access to Precise Location, in order to create a Geo Hash to " +
+                "store to the local database"
+            const val PermissionGrantedDescription = "Precise location permission is granted"
+            const val PermissionNotGrantedDescription = "Precise location permission is not granted"
         }
 
         object Background {
 
+            const val Name = "Background location permissions"
             const val Title = "Allow background location permissions"
-            const val Description = "The background location permissions is needed, in order to update the widget " +
-                "and the suggestions"
+            const val Description = "Shuttle needs access to Background Location, in order to create a Geo Hash to " +
+                "store to the local database every time an app has been opened. Additionally, Shuttle will use the " +
+                "current location in the Widget, in order to provide suggestions based on the current location"
+            const val PermissionGrantedDescription = "Background location permission is granted"
+            const val PermissionNotGrantedDescription = "Background location permission is not granted"
         }
     }
 }
---
+++ shuttle/permissions/src/main/kotlin/shuttle/permissions/ui/PermissionItem.kt
@@ -0,0 +1,137 @@
+package shuttle.permissions.ui
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.CheckCircle
+import androidx.compose.material.icons.rounded.Warning
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.vector.rememberVectorPainter
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import shuttle.design.PreviewDimens
+import shuttle.design.theme.Dimens
+import shuttle.design.theme.ShuttleTheme
+import shuttle.permissions.model.PermissionItemUiModel
+import shuttle.permissions.resources.Strings
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+internal fun PermissionItem(
+    permissionItem: PermissionItemUiModel,
+    onRequestPermission: () -> Unit
+) {
+    val containerColor =
+        if (permissionItem.isGranted()) MaterialTheme.colorScheme.secondaryContainer
+        else MaterialTheme.colorScheme.errorContainer
+    Card(
+        containerColor = containerColor,
+        modifier = Modifier
+            .padding(horizontal = Dimens.Margin.Medium, vertical = Dimens.Margin.Small)
+            .fillMaxWidth()
+    ) {
+        Box(
+            modifier = Modifier
+                .padding(horizontal = Dimens.Margin.Medium, vertical = Dimens.Margin.Small)
+                .fillMaxWidth()
+        ) {
+            when (permissionItem) {
+                is PermissionItemUiModel.Granted -> GrantedPermissionItem(permissionItem)
+                is PermissionItemUiModel.NotGranted -> NotGrantedPermissionItem(permissionItem, onRequestPermission)
+            }
+        }
+    }
+}
+
+@Composable
+private fun GrantedPermissionItem(permissionItem: PermissionItemUiModel.Granted) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Text(text = permissionItem.name, style = MaterialTheme.typography.titleLarge)
+            Icon(
+                painter = rememberVectorPainter(image = Icons.Rounded.CheckCircle),
+                tint = MaterialTheme.colorScheme.secondary,
+                contentDescription = permissionItem.permissionGrantedDescription,
+                modifier = Modifier.size(Dimens.Icon.Medium)
+            )
+        }
+        Spacer(modifier = Modifier.height(Dimens.Margin.Small))
+        Text(
+            text = permissionItem.permissionGrantedDescription,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.fillMaxWidth()
+        )
+    }
+}
+
+@Composable
+private fun NotGrantedPermissionItem(
+    permissionItem: PermissionItemUiModel.NotGranted,
+    onRequestPermission: () -> Unit
+) {
+    Column(modifier = Modifier.fillMaxWidth()) {
+        Row(horizontalArrangement = Arrangement.SpaceBetween, modifier = Modifier.fillMaxWidth()) {
+            Text(text = permissionItem.name, style = MaterialTheme.typography.titleLarge)
+            Icon(
+                painter = rememberVectorPainter(image = Icons.Rounded.Warning),
+                tint = MaterialTheme.colorScheme.error,
+                contentDescription = permissionItem.permissionNotGrantedDescription,
+                modifier = Modifier.size(Dimens.Icon.Medium)
+            )
+        }
+        Spacer(modifier = Modifier.height(Dimens.Margin.Small))
+        Text(
+            text = permissionItem.description,
+            style = MaterialTheme.typography.bodyMedium,
+            textAlign = TextAlign.Justify,
+            modifier = Modifier.fillMaxWidth()
+        )
+        Spacer(modifier = Modifier.height(Dimens.Margin.Small))
+        Row(horizontalArrangement = Arrangement.End, modifier = Modifier.fillMaxWidth()) {
+            Button(onClick = onRequestPermission) {
+                Text(text = permissionItem.buttonText)
+            }
+        }
+    }
+}
+
+@Composable
+@Preview(showBackground = true, widthDp = PreviewDimens.Medium.Width)
+private fun GrantedPermissionItemPreview() {
+    val uiModel = PermissionItemUiModel.Granted(
+        name = Strings.Location.Background.Name,
+        permissionGrantedDescription = Strings.Location.Background.PermissionGrantedDescription
+    )
+    ShuttleTheme {
+        PermissionItem(uiModel, onRequestPermission = {})
+    }
+}
+
+@Composable
+@Preview(showBackground = true, widthDp = PreviewDimens.Medium.Width)
+private fun NotGrantedPermissionItemPreview() {
+    val uiModel = PermissionItemUiModel.NotGranted(
+        name = Strings.Location.Background.Name,
+        description = Strings.Location.Background.Description,
+        permissionNotGrantedDescription = Strings.Location.Background.PermissionNotGrantedDescription,
+        buttonText = Strings.Location.Action
+    )
+    ShuttleTheme {
+        PermissionItem(uiModel, onRequestPermission = {})
+    }
+}
