07051e57057be2566d31a1e25cbcc4078f05ce89
4face-studi0 <4face.studio@gmail.com>
2022-03-13 14:07:05 +0100

    Remove unused Permissions resources

--- shuttle/permissions/src/main/kotlin/shuttle/permissions/resources/Strings.kt
+++ shuttle/permissions/src/main/kotlin/shuttle/permissions/resources/Strings.kt
@@ -5,7 +5,6 @@ internal object Strings {
     object Accessibility {
 
         const val Name = "Accessibility service"
-        const val Title = "Enable accessibility service"
         const val Description = "Shuttle needs access to the Accessibility Service, in order to improve the " +
             "suggestions.\nBy enabling it you consent Shuttle to retrieve your current open app and store it to the " +
             "local database"
@@ -21,7 +20,6 @@ internal object Strings {
         object Coarse {
 
             const val Name = "Location permissions"
-            const val Title = "Allow location permissions"
             const val Description = "Shuttle needs access to Location, in order to show suggested apps, based on " +
                 "your current location\nBy allowing the permission you consent Shuttle to store a Geo Hash of your " +
                 "current position and store it to the local database"
@@ -32,7 +30,6 @@ internal object Strings {
         object Fine {
 
             const val Name = "Precise location permissions"
-            const val Title = "Allow fine location permissions"
             const val Description = "Shuttle needs access to Precise Location, in order to create a Geo Hash to " +
                 "store to the local database"
             const val PermissionGrantedDescription = "Precise location permission is granted"
@@ -42,7 +39,6 @@ internal object Strings {
         object Background {
 
             const val Name = "Background location permissions"
-            const val Title = "Allow background location permissions"
             const val Description = "Shuttle needs access to Background Location, in order to create a Geo Hash to " +
                 "store to the local database every time an app has been opened. Additionally, Shuttle will use the " +
                 "current location in the Widget, in order to provide suggestions based on the current location"
--- shuttle/permissions/src/main/kotlin/shuttle/permissions/ui/PermissionsPage.kt
+++ shuttle/permissions/src/main/kotlin/shuttle/permissions/ui/PermissionsPage.kt
@@ -1,27 +1,13 @@
 package shuttle.permissions.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.style.TextAlign
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
 import com.google.accompanist.permissions.rememberMultiplePermissionsState
 import org.koin.androidx.compose.viewModel
-import shuttle.design.theme.Dimens
 import shuttle.design.ui.LoadingSpinner
 import shuttle.design.util.collectAsStateLifecycleAware
 import shuttle.permissions.model.PermissionItemsUiModel
@@ -91,34 +77,6 @@ private fun PermissionsList(
                 permissionItem = permissions.accessibilityService,
                 onRequestPermission = onRequestAccessibilityService
             )
-        }
-    }
-}
-
-@Composable
-internal fun RequestPermissionsPage(title: String, description: String, action: String, onAction: () -> Unit) {
-    Column(
-        verticalArrangement = Arrangement.SpaceBetween,
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(Dimens.Margin.XXLarge)
-    ) {
-        Column {
-            Text(
-                text = title,
-                style = MaterialTheme.typography.titleLarge
-            )
-            Spacer(modifier = Modifier.height(Dimens.Margin.XLarge))
-            Text(
-                text = description,
-                style = MaterialTheme.typography.bodyMedium,
-                textAlign = TextAlign.Justify,
-            )
-        }
-        Row(modifier = Modifier.fillMaxWidth(), horizontalArrangement = Arrangement.End) {
-            Button(onClick = onAction) {
-                Text(action)
-            }
         }
     }
 }
