29fdcf630d5f50a4bdaa3ac97105fdaee3a53f95
4face-studi0 <4face.studio@gmail.com>
2022-03-14 16:54:29 +0100

    Add changesNotSentForReview to deploy.yml

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ shuttleAndroid {
     useCompose()
     androidApp(
         id = "studio.forface.shuttle",
-        versionCode = version.toInt() + 105,
+        versionCode = version.toInt() + 100,
         versionName = version
     )
 }
