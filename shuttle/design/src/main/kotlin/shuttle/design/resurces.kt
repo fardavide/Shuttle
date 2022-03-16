a45c8661999be57abd9f25cf4d04d004f8553d86
4face-studi0 <4face.studio@gmail.com>
2022-03-16 18:50:07 +0100

    Add StringResource

---
+++ shuttle/design/src/main/kotlin/shuttle/design/resurces.kt
@@ -0,0 +1,25 @@
+package shuttle.design
+
+import android.content.res.Resources
+import androidx.core.os.ConfigurationCompat
+import kotlin.reflect.KProperty1
+
+typealias StringResource <T> = KProperty1<T, String>
+
+fun getLanguage(): SupportedLanguage {
+    val language = ConfigurationCompat.getLocales(Resources.getSystem().configuration)
+        .get(0)
+        .language
+
+    return when (language) {
+        "en" -> SupportedLanguage.English
+        "it" -> SupportedLanguage.Italian
+        else -> SupportedLanguage.English
+    }
+}
+
+enum class SupportedLanguage {
+
+    English,
+    Italian
+}
