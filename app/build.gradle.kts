d91a980312b9084a77453ed78cd2dd5e2bebc672
4face-studi0 <4face.studio@gmail.com>
2022-03-30 12:04:43 +0200

    Reduce version diff to 20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ shuttleAndroid {
     useCompose()
     androidApp(
         id = "studio.forface.shuttle",
-        versionCode = version.toInt() + 30,
+        versionCode = version.toInt() + 20,
         versionName = version
     )
 }
