549825b3bf6eb6f4e288fdf7cf055913d29b80ab
4face-studi0 <4face.studio@gmail.com>
2022-04-24 15:36:56 +0200

    Setup Logger in RefreshLocationWorker.kt

--- shuttle/coordinates/data/src/main/kotlin/shuttle/coordinates/data/worker/RefreshLocationWorker.kt
+++ shuttle/coordinates/data/src/main/kotlin/shuttle/coordinates/data/worker/RefreshLocationWorker.kt
@@ -6,13 +6,15 @@ import androidx.work.ExistingPeriodicWorkPolicy
 import androidx.work.PeriodicWorkRequestBuilder
 import androidx.work.WorkManager
 import androidx.work.WorkerParameters
+import co.touchlab.kermit.Logger
 import shuttle.coordinates.domain.CoordinatesRepository
 import kotlin.time.Duration
 
 internal class RefreshLocationWorker(
     appContext: Context,
     params: WorkerParameters,
-    private val coordinatesRepository: CoordinatesRepository
+    private val coordinatesRepository: CoordinatesRepository,
+    private val logger: Logger
 ) : CoroutineWorker(appContext, params) {
 
     override suspend fun doWork(): Result {
