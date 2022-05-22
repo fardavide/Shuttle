54cb45b2865559021803528b57e711f27d92b8c2
4face-studi0 <4face.studio@gmail.com>
2022-05-22 08:40:53 +0200

    Add elevation to BottomSheet.kt

--- shuttle/design/src/main/kotlin/shuttle/design/ui/BottomSheet.kt
+++ shuttle/design/src/main/kotlin/shuttle/design/ui/BottomSheet.kt
@@ -1,8 +1,10 @@
 package shuttle.design.ui
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.CardElevation
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FabPosition
 import androidx.compose.material3.MaterialTheme
@@ -10,8 +12,9 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.composed
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
+import androidx.compose.ui.unit.dp
 
 @ExperimentalMaterial3Api
 @Composable
@@ -22,27 +25,39 @@ fun BottomSheetScaffold(
     snackbarHost: @Composable () -> Unit = {},
     floatingActionButton: @Composable () -> Unit = {},
     floatingActionButtonPosition: FabPosition = FabPosition.End,
-    containerColor: Color = Color.Transparent,
+    containerColor: Color = MaterialTheme.colorScheme.background,
     contentColor: Color = contentColorFor(containerColor),
+    elevation: CardElevation = BottomSheetDefaults.bottomSheetElevation(),
+    shape: Shape = BottomSheetDefaults.bottomSheetShape(),
     content: @Composable (PaddingValues) -> Unit
 ) {
-    Scaffold(
-        modifier = modifier.bottomSheetBackground(),
-        topBar = topBar,
-        bottomBar = bottomBar,
-        snackbarHost = snackbarHost,
-        floatingActionButton = floatingActionButton,
-        floatingActionButtonPosition = floatingActionButtonPosition,
-        containerColor = containerColor,
-        contentColor = contentColor,
-        content = content
-    )
+    Card(
+        modifier = modifier,
+        elevation = elevation,
+        shape = shape
+    ) {
+        Scaffold(
+            modifier = Modifier,
+            topBar = topBar,
+            bottomBar = bottomBar,
+            snackbarHost = snackbarHost,
+            floatingActionButton = floatingActionButton,
+            floatingActionButtonPosition = floatingActionButtonPosition,
+            containerColor = containerColor,
+            contentColor = contentColor,
+            content = content
+        )
+    }
 }
 
-fun Modifier.bottomSheetBackground(): Modifier = composed {
-    val shape = MaterialTheme.shapes.extraLarge.copy(
+object BottomSheetDefaults {
+
+    @Composable
+    fun bottomSheetShape() = MaterialTheme.shapes.extraLarge.copy(
         bottomStart = CornerSize(0),
         bottomEnd = CornerSize(0)
     )
-    background(color = MaterialTheme.colorScheme.surface, shape = shape)
+
+    @Composable
+    fun bottomSheetElevation() = CardDefaults.elevatedCardElevation(defaultElevation = 8.dp)
 }
