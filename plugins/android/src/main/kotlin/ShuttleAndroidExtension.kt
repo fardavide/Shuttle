dccc6daa9180bddde480119283f841d5d0277500
4face-studi0 <4face.studio@gmail.com>
2022-06-13 08:31:48 +0200

    Add Firebase Test Lab action

--- plugins/android/src/main/kotlin/ShuttleAndroidExtension.kt
+++ plugins/android/src/main/kotlin/ShuttleAndroidExtension.kt
@@ -2,7 +2,6 @@ import com.android.build.gradle.TestedExtension
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
 import org.gradle.kotlin.dsl.create
-import org.gradle.kotlin.dsl.extra
 import javax.inject.Inject
 
 open class ShuttleAndroidExtension @Inject constructor(private val project: Project) {
