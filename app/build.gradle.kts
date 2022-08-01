355f56b24fae7334f018163cff31b0716a60f782
Davide Farella <fardavide@pm.me>
2022-08-01 11:53:15 +0200

    Update App version with new runner counter

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,7 +7,7 @@ shuttleAndroid {
     useCompose()
     androidApp(
         id = "studio.forface.shuttle",
-        versionCode = version.toInt(),
+        versionCode = version.toInt() + 166,
         versionName = version
     )
 }
