786912b869eab52e9d47262ce3263e1ab8910217
Davide Giuseppe Farella <fardavide@gmail.com>
2023-02-12 12:43:22 +0100

    feat(app): set log as StrictMode penalty

--- app/src/main/java/studio/forface/shuttle/startup/StrictModeStartup.kt
+++ app/src/main/java/studio/forface/shuttle/startup/StrictModeStartup.kt
@@ -7,8 +7,8 @@ object StrictModeStartup : Startup {
 
     override fun init() {
         if (BuildConfig.DEBUG) {
-            val threadPolicy = StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().penaltyDeath().build()
-            val vmPolicy = StrictMode.VmPolicy.Builder().detectAll().penaltyLog().penaltyDeath().build()
+            val threadPolicy = StrictMode.ThreadPolicy.Builder().detectAll().penaltyLog().build()
+            val vmPolicy = StrictMode.VmPolicy.Builder().detectAll().penaltyLog().build()
             StrictMode.setThreadPolicy(threadPolicy)
             StrictMode.setVmPolicy(vmPolicy)
         }
