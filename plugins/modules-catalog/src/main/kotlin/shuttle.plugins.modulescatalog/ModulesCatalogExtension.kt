e01aea1c55d3735360357011c4747d3f491d447f
Davide Giuseppe Farella <fardavide@gmail.com>
2023-05-13 09:20:40 +0200

    chore(build): add api, testImplementation and androidTestImplementation to ModulesCatalogExtension

--- plugins/modules-catalog/src/main/kotlin/shuttle.plugins.modulescatalog/ModulesCatalogExtension.kt
+++ plugins/modules-catalog/src/main/kotlin/shuttle.plugins.modulescatalog/ModulesCatalogExtension.kt
@@ -6,8 +6,20 @@ import javax.inject.Inject
 
 open class ModulesCatalogExtension @Inject constructor(private val project: Project) {
 
+    fun api(module: String) {
+        project.dependencies.add("api", project.project(":shuttle:$module"))
+    }
+
     fun implementation(module: String) {
         project.dependencies.add("implementation", project.project(":shuttle:$module"))
+    }
+
+    fun testImplementation(module: String) {
+        project.dependencies.add("testImplementation", project.project(":shuttle:$module"))
+    }
+
+    fun androidTestImplementation(module: String) {
+        project.dependencies.add("androidTestImplementation", project.project(":shuttle:$module"))
     }
 
     companion object {
