5bc6c046e77ec0dd4fb4354bbb5f7f7dc3eb4c01
Davide Giuseppe Farella <fardavide@gmail.com>
2023-05-13 17:31:47 +0200

    feat(theme): use default Material Shapes

--- shuttle/design/src/main/kotlin/shuttle/design/theme/Shape.kt
+++ shuttle/design/src/main/kotlin/shuttle/design/theme/Shape.kt
@@ -1,13 +1,12 @@
 package shuttle.design.theme
 
-import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ShapeDefaults
 import androidx.compose.material3.Shapes
-import androidx.compose.ui.unit.dp
 
 val Shapes = Shapes(
-    extraSmall = RoundedCornerShape(12.dp),
-    small = RoundedCornerShape(24.dp),
-    medium = RoundedCornerShape(24.dp),
-    large = RoundedCornerShape(32.dp),
-    extraLarge = RoundedCornerShape(32.dp)
+    extraSmall = ShapeDefaults.ExtraSmall,
+    small = ShapeDefaults.Small,
+    medium = ShapeDefaults.Medium,
+    large = ShapeDefaults.Large,
+    extraLarge = ShapeDefaults.ExtraLarge
 )
