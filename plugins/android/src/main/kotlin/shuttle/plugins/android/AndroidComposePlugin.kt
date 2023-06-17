988f03389aa30a18cd9d8903438af9bae4c43afc
Davide Giuseppe Farella <fardavide@gmail.com>
2023-06-17 10:40:28 +0200

    fix: add suppressKotlinVersionCompatibilityCheck=1.8.22

--- plugins/android/src/main/kotlin/shuttle/plugins/android/AndroidComposePlugin.kt
+++ plugins/android/src/main/kotlin/shuttle/plugins/android/AndroidComposePlugin.kt
@@ -4,8 +4,10 @@ import com.android.build.gradle.TestedExtension
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import shuttle.plugins.util.configure
 import shuttle.plugins.util.libsCatalog
+import shuttle.plugins.util.withType
 
 internal class AndroidComposePlugin : Plugin<Project> {
 
@@ -13,11 +15,21 @@ internal class AndroidComposePlugin : Plugin<Project> {
         val libsCatalog = target.libsCatalog
         target.extensions.configure<TestedExtension> { ext -> configureComposeOptions(libsCatalog, ext) }
         target.pluginManager.apply("app.cash.molecule")
+        target.tasks.withType<KotlinCompile> { task ->
+            task.kotlinOptions {
+                freeCompilerArgs += listOf(
+                    // TODO: Remove this once we upgrade Molecule to support Kotlin 1.8.22
+                    "-P",
+                    "plugin:androidx.compose.compiler.plugins.kotlin:suppressKotlinVersionCompatibilityCheck=1.8.22"
+                )
+            }
+        }
     }
 
     @Suppress("UnstableApiUsage")
     private fun configureComposeOptions(libsCatalog: VersionCatalog, ext: TestedExtension) {
         ext.buildFeatures.compose = true
-        ext.composeOptions.kotlinCompilerExtensionVersion = libsCatalog.findVersion("composeCompiler").get().toString()
+        val composeCompilerVersion = libsCatalog.findVersion("composeCompiler").get().toString()
+        ext.composeOptions.kotlinCompilerExtensionVersion = composeCompilerVersion
     }
 }
