9657e407ceb67fa3758f1792fbe45f3fc5c8c0a6
Davide Giuseppe Farella <fardavide@gmail.com>
2023-06-29 12:28:27 +0200

    chore: using Duration extensions

--- shuttle/stats/domain/src/main/kotlin/shuttle/stats/domain/StatsDomainModule.kt
+++ shuttle/stats/domain/src/main/kotlin/shuttle/stats/domain/StatsDomainModule.kt
@@ -4,8 +4,8 @@ import org.koin.core.annotation.ComponentScan
 import org.koin.core.annotation.Factory
 import org.koin.core.annotation.Module
 import org.koin.core.annotation.Named
-import kotlin.time.DurationUnit
-import kotlin.time.toDuration
+import kotlin.time.Duration.Companion.days
+import kotlin.time.Duration.Companion.hours
 
 @Module
 @ComponentScan
@@ -13,11 +13,11 @@ class StatsDomainModule {
 
     @Factory
     @Named(StatsQualifier.Interval.DeleteOldStats.Scheduler.Default)
-    fun intervalDeleteOldStatsSchedulerDefault() = 12.toDuration(DurationUnit.HOURS)
+    fun intervalDeleteOldStatsSchedulerDefault() = 12.hours
 
     @Factory
     @Named(StatsQualifier.Interval.DeleteOldStats.Scheduler.Flex)
-    fun intervalDeleteOldStatsSchedulerFlex() = 1.toDuration(DurationUnit.DAYS)
+    fun intervalDeleteOldStatsSchedulerFlex() = 1.days
 }
 
 object StatsQualifier {
