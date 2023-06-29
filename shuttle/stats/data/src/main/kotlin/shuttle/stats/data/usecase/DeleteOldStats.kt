8e5902dbfd768409deb5b864df492467d588ecfd
Davide Giuseppe Farella <37957092+fardavide@users.noreply.github.com>
2023-06-29 23:35:07 +0200

    fix: don't delete stats if to keep forever

--- shuttle/stats/data/src/main/kotlin/shuttle/stats/data/usecase/DeleteOldStats.kt
+++ shuttle/stats/data/src/main/kotlin/shuttle/stats/data/usecase/DeleteOldStats.kt
@@ -4,6 +4,7 @@ import kotlinx.coroutines.flow.first
 import org.koin.core.annotation.Factory
 import shuttle.coordinates.domain.usecase.ObserveCurrentDateTime
 import shuttle.database.datasource.StatDataSource
+import shuttle.settings.domain.model.KeepStatisticsFor
 import shuttle.settings.domain.usecase.ObserveKeepStatisticsFor
 import shuttle.stats.data.mapper.DatabaseDateAndTimeMapper
 import shuttle.utils.kotlin.minus
@@ -17,9 +18,12 @@ internal class DeleteOldStats(
 ) {
 
     suspend operator fun invoke() {
+        val toKeep = observeKeepStatisticsFor().first()
+            .takeIf { it !is KeepStatisticsFor.Forever }
+            ?.duration
+            ?: return
         val currentDate = observeCurrentDateTime().first().date
-        val toKeep = observeKeepStatisticsFor().first().duration
-
+   
         val databaseDate = databaseDateAndTimeMapper.toDatabaseDate(currentDate - toKeep)
         statDataSource.clearAllStatsOlderThan(date = databaseDate)
     }
