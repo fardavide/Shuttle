0906a79254547dd3fda480220c60ea21a00cfb02
Davide Giuseppe Farella <fardavide@gmail.com>
2023-06-29 12:12:03 +0200

    feat(settings): use keep statistics for in DeleteOldStats

--- shuttle/stats/data/src/main/kotlin/shuttle/stats/data/usecase/DeleteOldStats.kt
+++ shuttle/stats/data/src/main/kotlin/shuttle/stats/data/usecase/DeleteOldStats.kt
@@ -4,24 +4,23 @@ import kotlinx.coroutines.flow.first
 import org.koin.core.annotation.Factory
 import shuttle.coordinates.domain.usecase.ObserveCurrentDateTime
 import shuttle.database.datasource.StatDataSource
+import shuttle.settings.domain.usecase.ObserveKeepStatisticsFor
 import shuttle.stats.data.mapper.DatabaseDateAndTimeMapper
+import shuttle.utils.kotlin.minus
 
 @Factory
 internal class DeleteOldStats(
     private val databaseDateAndTimeMapper: DatabaseDateAndTimeMapper,
     private val observeCurrentDateTime: ObserveCurrentDateTime,
+    private val observeKeepStatisticsFor: ObserveKeepStatisticsFor,
     private val statDataSource: StatDataSource
 ) {
 
     suspend operator fun invoke() {
-        statDataSource
-        val currentDate = databaseDateAndTimeMapper.toDatabaseDate(observeCurrentDateTime().first().date)
+        val currentDate = observeCurrentDateTime().first().date
+        val toKeep = observeKeepStatisticsFor().first().duration
 
-        statDataSource.clearAllStatsOlderThan(date = currentDate - DaysToKeep)
-    }
-
-    companion object {
-
-        private const val DaysToKeep = 90
+        val databaseDate = databaseDateAndTimeMapper.toDatabaseDate(currentDate - toKeep)
+        statDataSource.clearAllStatsOlderThan(date = databaseDate)
     }
 }
--- shuttle/utils/kotlin/src/main/kotlin/shuttle/utils/kotlin/TimeUtils.kt
+++ shuttle/utils/kotlin/src/main/kotlin/shuttle/utils/kotlin/TimeUtils.kt
@@ -1,6 +1,15 @@
 package shuttle.utils.kotlin
 
+import korlibs.time.Date
+import korlibs.time.DateTime
+import korlibs.time.TimeSpan
+import korlibs.time.minus
 import kotlin.time.Duration
 
 val Duration.inWholeMonths: Int
     get() = inWholeDays.toInt() / 30
+
+operator fun Date.minus(duration: Duration): Date = minus(duration.toTimeSpan())
+operator fun DateTime.minus(duration: Duration): DateTime = minus(duration.toTimeSpan())
+
+fun Duration.toTimeSpan(): TimeSpan = TimeSpan(inWholeMilliseconds.toDouble())
