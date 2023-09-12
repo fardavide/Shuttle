f8cdbaefe06cc9a0b4e810fd7bf04f3cc8f1d8f0
Davide Giuseppe Farella <fardavide@gmail.com>
2023-09-12 20:49:56 +0200

    feat(launchers): add Launchers.NovaLauncher

--- shuttle/launchers/src/main/kotlin/shuttle/launchers/Launchers.kt
+++ shuttle/launchers/src/main/kotlin/shuttle/launchers/Launchers.kt
@@ -5,12 +5,14 @@ object Launchers {
     private const val AndroidSettings = "com.android.settings"
     private const val MotorolaLauncher = "com.motorola.launcher3"
     private const val NexusLauncher = "com.google.android.apps.nexuslauncher"
+    private const val NovaLauncher = "com.teslacoilsw.launcher"
     private const val OneUiLauncher = "com.sec.android.app.launcher"
 
     fun all(): List<String> = listOf(
         AndroidSettings,
         MotorolaLauncher,
         NexusLauncher,
+        NovaLauncher,
         OneUiLauncher
     )
 }
